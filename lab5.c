7d7f1fac9c26e69f169dedab5ee048cf80002c35
tkohlhauff <tkohlhauff@gmail.com>
2017-02-23 12:20:58 -0600

    Add files via upload

--- lab5.c
+++ lab5.c
@@ -1,55 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//Check leap year for given year
 int leap(int x)
 {
     if(x%4==0 && x%100!=0 || x%400==0){
-		printf("\nhere");
         return 1;
 	}
     else
         return 0;
 }
-
-int getDate(int *m, int *d,int *y)
+//calDate takes days passed and finds the date for it in that year
+int calDate(int *m, int *d,int *y)
 {
 	int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
 	int temp=0;
 	int x=0;
-	if (leap(*y)){
+	//Check leap year
+	if (leap(*y)==1){
 		days[1]=29;
 	}
+	//Find the month it is in
 	while(*d>temp){
 		temp=days[x]+temp;
 		x++;
 	}
+	//Subtract total days from amount of months from input days passed
 	if(temp-*d>0){
 		temp=temp-*d;
-		*d=days[x]-temp;
-		printf("\n%d",*d);
+		//Then subtact the months days from the remainder
+		*d=days[x-1]-temp;
 	}
 	else{
-		*d=days[x];
+		//If the given days passed are equal to the sum of the months passed just set the date to the last day in the month
+		*d=days[x-1];
 	}
 	*m=x;
 }
-
+//calDays calculates total days passed from given date from user
 int calDays(int m, int d, int y)
 {
     int days[]={31,28,31,30,31,30,31,31,30,31,30,31};
 	int i;
 	int tot=0;
-
-	if (leap(y)){
+	//Check leap year
+	if (leap(y)==1){
 		days[1]=29;
 	}
+	//Check for user input error
 	if (d>days[m-1]){
 		printf("\nPlease enter a correct date");
 		return 0;
 	}
+	//adds each month days to total
 	for(i=0;i<m-1;i++)
 		tot=tot+days[i];
+	//Add the given date to the sum of the months
+	tot=tot+d;
 	return tot;
 }
 
@@ -58,14 +65,16 @@ int main()
 	
 	char more='y';
 	int mm,dd,yy, choice;
+	//Loop for user to do infinite inputs
 	while(more=='y' || more =='Y'){
 		printf("This program will find days passed or date in the year");
 		printf("\n1) Input date (mm/dd/yyyy) to find days passed");
 		printf("\n2) Input passed days to find date in the year");
+		//Asks user which problem they would like to solve
 		printf("\nYour choice (1/2): ");
 		scanf("%d",&choice);
 		if(choice==1){
-			printf("\nPlease input date (mm-dd-yy): ");
+			printf("\nPlease input date (mm-dd-yyyy): ");
 			scanf("%d-%d-%d",&mm,&dd,&yy);
 			printf("\nThere are %d days passed in the year %d",calDays(mm,dd,yy),yy);
 		}
@@ -74,9 +83,10 @@ int main()
 			scanf("%d",&dd);
 			printf("\nYear: ");
 			scanf("%d",&yy);
-			getDate(&mm,&dd,&yy);
+			calDate(&mm,&dd,&yy);
 			printf("\nThe date is %d-%d-%d",mm,dd,yy);
 		}
+		//Asks user if they would to calculate something else
 		printf("\nWould you like to do more?(Y/N): ");
 		scanf("%s",&more);
 	}
